91dd649ab53b9b208a2cff899e0b7cfc69e97e5b
Fatih Beyhan <fbeyhan@gmail.com>
2019-11-18 23:11:33 -0500

    this is commit from githubpractice2

---
+++ src/test/java/com/geico/pages/login_navigation/AutoInsurance.java
@@ -0,0 +1,11 @@
+package com.geico.pages.login_navigation;
+
+import org.junit.Test;
+
+public class AutoInsurance {
+
+    @Test
+    public void test1(){
+        System.out.println("test1");
+    }
+}
