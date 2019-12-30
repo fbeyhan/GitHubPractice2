80fcfd9f33ef8e1793b9de3fec61e4e38d5c2d4b
Fatih Beyhan <fbeyhan@gmail.com>
2019-12-29 21:26:54 -0500

    update fatih test

--- src/test/java/com/geico/githubFatihPractice/GitHubPracticeClass2.java
+++ src/test/java/com/geico/githubFatihPractice/GitHubPracticeClass2.java
@@ -1,4 +1,10 @@
 package com.geico.githubFatihPractice;
 
+import org.junit.Test;
+
 public class GitHubPracticeClass2 {
+    @Test
+    public void fatih(){
+        System.out.println("fatih1");
+    }
 }
