0578e7dfc19cc73fa6450fa00c10580181da4846
Fatih Beyhan <fbeyhan@gmail.com>
2019-12-30 20:21:28 -0500

    adding GitHubPracticeClass5

---
+++ src/test/java/com/geico/githubFatihPractice/GitHubPracticeClass5.java
@@ -0,0 +1,10 @@
+package com.geico.githubFatihPractice;
+
+import org.junit.Test;
+
+public class GitHubPracticeClass5 {
+    @Test
+    public void fatih5(){
+        System.out.println("fatih5");
+    }
+}
