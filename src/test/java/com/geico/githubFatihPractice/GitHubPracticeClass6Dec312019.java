4d325af667ebcfd01646182adf5e37e56f81edce
Fatih Beyhan <fbeyhan@gmail.com>
2019-12-31 20:12:08 -0500

    Dec312019 update adding class6

---
+++ src/test/java/com/geico/githubFatihPractice/GitHubPracticeClass6Dec312019.java
@@ -0,0 +1,10 @@
+package com.geico.githubFatihPractice;
+
+import org.junit.Test;
+
+public class GitHubPracticeClass6Dec312019 {
+    @Test
+    public void fatih6(){
+        System.out.println("fatih6");
+    }
+}
